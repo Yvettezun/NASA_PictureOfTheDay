96cf7d4337d8e8e66dd7b9ee3c23cf746c042b61
Yvette <yvettezuniga5@gmail.com>
2022-11-02 22:23:18 -0500

    adds fetch

--- script.js
+++ script.js
@@ -3,8 +3,35 @@
 let imageBox = document.getElementById("nasaImage");
 let dateBox = document.getElementById("dateBox");
 
-dateBox.onchange = function() {   
+dateBox.onchange = async function() {   
+let dateStr=dateBox.value;
+let url = "https://api.nasa.gov/planetary/apod?api_key=";
+let key = 'DEMO_KEY';
 
+    try{
+       let response= await fetch(`${url}${key}&date=${dateStr}`)
+        if(response.ok){
+            let json= await response.json();
+            showPicture(json);
+        }
+        else{
+            throw new Error('An error occurred while fetching');
+        }
+    }catch(error){
+        console.log(error);
+    }
+}
+
+function showPicture(json){
+    if(json.media_type==='video'){
+        imageBox.innerHTML=  `<iframe src='${json.url}'></iframe><h1>${json.title}</h1><p>${json.explanation}</p>`;  
+    }
+    else if(json.media_type==='image'){
+        imageBox.innerHTML=`<img src='${json.url}'/><h1>${json.title}</h1><p>${json.explanation}</p>`;
+    }
+    else{
+        imageBox.innerHTML='Image not Available';
+    }
 }
 
 
